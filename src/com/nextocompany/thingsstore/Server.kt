238fc1a5f85cb2f9d3509bba5f6e10ea219338a7
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-11-22 19:40:33 +0100

    Added server for tesing.

--- src/com/nextocompany/thingsstore/Server.kt
+++ src/com/nextocompany/thingsstore/Server.kt
@@ -1,20 +1,26 @@
 package com.nextocompany.thingsstore
 
-import java.net.InetAddress
 import java.net.ServerSocket
 import java.net.Socket
 
-const val serverPort: Int = 488
+const val serverPort: Int = 2048
 
 private val serverSocket: ServerSocket = ServerSocket(serverPort)
 
 fun main(args: Array<String>) {
     while (true) {
         val clientSocket: Socket = serverSocket.accept()
-        print("Nuova connessione socket!")
+
+        println("\nConnessione in ingresso da: " + clientSocket.inetAddress.hostAddress)
+        println("Avvio gestione della connessione!")
+
         Thread {
-            val clientAddress: InetAddress = clientSocket.inetAddress
-            System.out.println("Connessione in ingresso da: " + clientAddress.hostAddress + "]")
+            when (clientSocket.getInputStream().read()) {
+                1 -> println("Inviato 1!")
+                2 -> println("Inviato 2!")
+                else -> println("Errore!")
+            }
+
             clientSocket.close()
         }
     }
--- src/com/nextocompany/thingsstore/ServerTest.kt
+++ src/com/nextocompany/thingsstore/ServerTest.kt
@@ -1,5 +1,9 @@
 package com.nextocompany.thingsstore
 
-fun main(args: Array<String>) {
+import java.net.Socket
 
+private val clientSocket: Socket = Socket("Pi-3.local", 2048)
+
+fun main(args: Array<String>) {
+    clientSocket.getOutputStream().write(-3)
 }
