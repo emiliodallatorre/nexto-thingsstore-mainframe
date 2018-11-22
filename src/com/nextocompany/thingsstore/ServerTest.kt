a2e114ab6d95807db6819a2597c2bd955c56d011
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-11-22 19:59:21 +0100

    Created test server socket.

--- src/com/nextocompany/thingsstore/ServerTest.kt
+++ src/com/nextocompany/thingsstore/ServerTest.kt
@@ -5,5 +5,5 @@ import java.net.Socket
 private val clientSocket: Socket = Socket("Pi-3.local", 2048)
 
 fun main(args: Array<String>) {
-    clientSocket.getOutputStream().write(-3)
+    clientSocket.getOutputStream().write(2)
 }
