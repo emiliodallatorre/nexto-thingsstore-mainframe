09c11a3cc31a1ee1c6c506a8d5fc9cb33c2cc413
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-11-24 18:16:55 +0100

    Removed useless test class.

--- src/com/nextocompany/thingsstore/ServerTest.kt
+++
@@ -1,9 +0,0 @@
-package com.nextocompany.thingsstore
-
-import java.net.Socket
-
-private val clientSocket: Socket = Socket("Pi-3.local", 2048)
-
-fun main(args: Array<String>) {
-    clientSocket.getOutputStream().write(2)
-}
