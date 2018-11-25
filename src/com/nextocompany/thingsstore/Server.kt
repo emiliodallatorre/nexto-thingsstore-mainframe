c01ef1f95cef12faf8f6d7a86defe2d81087ba13
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-11-25 14:52:55 +0100

    Fixed server bootstrap.

--- src/com/nextocompany/thingsstore/Server.kt
+++ src/com/nextocompany/thingsstore/Server.kt
@@ -7,7 +7,11 @@ val session: ServerSession = ServerSession
 fun main(args: Array<String>) {
     session.initializer = ServerInitializer()
 
-    session.listener.start()
+    session.initializer.initAll()
+
+    session.listener.startListening()
+
+    session.scanner = Scanner(System.`in`)
 
     while (true) {
         when (session.scanner.nextLine()) {
@@ -15,6 +19,8 @@ fun main(args: Array<String>) {
             "start" -> session.controls.start()
             "verbose" -> session.controls.verbose()
             "status" -> session.controls.status()
+            "disconnetti" -> session.controls.disconnectAll()
+            "log" -> session.controls.log()
             else -> session.logger.log("Errore, riprovare!\n", References.LEVEL_SERVER)
         }
     }
