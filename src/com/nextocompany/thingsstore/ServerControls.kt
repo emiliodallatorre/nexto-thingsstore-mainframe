8f5963a5f01cae8d2a60a2d0675036c021a382cb
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-11-25 14:52:05 +0100

    Added disconnectAll() function.

--- src/com/nextocompany/thingsstore/ServerControls.kt
+++ src/com/nextocompany/thingsstore/ServerControls.kt
@@ -25,14 +25,40 @@ class ServerControls {
     fun stop() {
         session.logger.log("Tentativo di disattivare l\'ascolto di nuove connessioni.", References.LEVEL_SERVER)
 
-        if (session.serverStatus == References.STATUS_STOPPED) session.logger.log("Il server è già inattivo.", References.LEVEL_MESSAGE)
-        else session.serverStatus = References.STATUS_STOPPED
+        if (session.serverStatus == References.STATUS_STOPPED) session.logger.log(
+            "Il server è già inattivo.",
+            References.LEVEL_MESSAGE
+        )
+        else session.listener.stopListening()
     }
 
     fun start() {
         session.logger.log("Tentativo di attivare l\'ascolto di nuove connessioni.", References.LEVEL_SERVER)
 
-        if (session.serverStatus == References.STATUS_RUNNING) session.logger.log("Il server è già attivo.", References.LEVEL_MESSAGE)
-        else session.serverStatus = References.STATUS_RUNNING
+        if (session.serverStatus == References.STATUS_RUNNING) session.logger.log(
+            "Il server è già attivo.",
+            References.LEVEL_MESSAGE
+        )
+        else session.listener.startListening()
+    }
+
+    fun disconnectAll() {
+        session.logger.log("Avvio disconnessione degli utenti.", References.LEVEL_SERVER)
+        Thread().run {
+            session.listener.executor.shutdown()
+            while (!session.listener.executor.isTerminated) {
+                Thread.sleep(References.SERVER_REFRESHRATE)
+            }
+
+            session.logger.log("Tutti gli utenti sono stati disconnessi correttamente.", References.LEVEL_WARNING)
+            session.logger.log("Payout del server: " + session.listener.executor, References.LEVEL_LOG)
+
+            session.initializer.initListener()
+        }
+    }
+
+    fun log() {
+        session.logger.log("Inserire testo da loggare nel file: ", References.LEVEL_MESSAGE)
+        session.logger.log("# USER: " + session.scanner.nextLine(), References.LEVEL_FILE)
     }
 }
