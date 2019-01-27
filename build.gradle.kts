72473e3cfd72db3ae726dc8ce945cc29887429d3
Emilio Dalla Torre <emiliodallatorre12@live.com>
2019-01-27 11:38:01 +0100

    Riconfigurato il file di configurazione di gradle.
    Non so cosa sia cambiato, ma così funziona comunque e sembra più aggiornato, stando alla documentazione di Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,31 @@
-apply plugin: 'kotlin'
-apply plugin: 'application'
-
-buildscript {
-    ext.kotlin_version = "1.3.20"
-
-    repositories {
-        jcenter()
-    }
-
-    dependencies {
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-    }
-
-}
-
-mainClassName = 'com.nextocompany.thingsstore.ServerKt'
-defaultTasks 'run'
-
-run {
-    standardInput = System.in
-}
-
-jar {
-    baseName = 'things-store'
-    version = '0.1'
+plugins {
+    kotlin("jvm") version "1.3.20"
+    application
+    `build-scan`
 }
 
 repositories {
     jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    compile "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
-    compile files('lib/mysql-connector-java-8.0.13.jar')
-
     // Test dependencies.
     testCompile("org.junit.jupiter:junit-jupiter-api:5.3.2")
     testCompile("org.junit.jupiter:junit-jupiter-params:5.3.2")
     testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.2")
+
+    implementation(kotlin("stdlib"))
+
+    // Aggiunge le dipendenze in formato .jar che stanno in ./lib.
+    implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
+}
+
+buildScan {
+    setTermsOfServiceUrl("https://gradle.com/terms-of-service")
+    setTermsOfServiceAgree("yes")
+
+    publishAlways()
 }
 
 tasks.withType<Test> {
@@ -50,3 +38,18 @@ tasks.withType<Test> {
 tasks.withType<Wrapper> {
     gradleVersion = "4.8"
 }
+
+allprojects {
+    group = "com.nextocompany.thingsstore"
+    version = "0.1"
+}
+
+application {
+    mainClassName = "com.nextocompany.thingsstore.ServerKt"
+}
+
+val run by tasks.getting(JavaExec::class) {
+    standardInput = System.`in`
+}
+
+defaultTasks("run")
